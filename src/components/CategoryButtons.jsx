1f1db92c33746a14b389a86a19efdead305a0eb3
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-03-23 08:30:18 -0300

    feat: component CategoryButtons

---
+++ src/components/CategoryButtons.jsx
@@ -0,0 +1,16 @@
+export default function CategoryButtons(){
+
+    return(
+
+        <div className="categories">
+
+            <button>🐶 Cachorros</button>
+            <button>🐱 Gatos</button>
+            <button>🐒 Macacos</button>
+            <button>🦜 Pássaros</button>
+            <button>📍 Petshops</button>
+
+        </div>
+
+    )
+}
