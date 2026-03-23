464f2a748cf999da77a3b5ad220829ec77b46d84
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-03-23 08:35:24 -0300

    fix: importing searchpage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './pages/src/style.css';
+import SearchPage from "./pages/src/SearchPage.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SearchPage />
   </React.StrictMode>
 );
