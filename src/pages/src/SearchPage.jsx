ec432fd213f242d34183a8381dcda9d8be9c5a2b
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-03-23 08:30:57 -0300

    feat: page and search.css

---
+++ src/pages/src/SearchPage.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import Header from "../../components/Header.jsx"
+import CategoryButtons from "../../components/CategoryButtons"
+import SearchBar from "../../components/SearchBar"
+import SearchResult from "../../components/SearchResult"
+
+export default function SearchPage() {
+
+}
