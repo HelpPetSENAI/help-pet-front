99468376225b47fac28157a78de26489cae80ab0
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-03-23 10:33:41 -0300

    fix: fixing main

--- src/pages/src/SearchPage.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-import Header from "../../components/Header.jsx"
-import CategoryButtons from "../../components/CategoryButtons"
-import SearchBar from "../../components/SearchBar"
-import SearchResult from "../../components/SearchResult"
-
-export default function SearchPage() {
-
-}
