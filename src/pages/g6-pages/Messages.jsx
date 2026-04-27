6fc8531a9990d5acb5bf74fd39fa97873975e6a5
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-04-27 11:06:12 -0300

    refactor: change file name to "Messages"

---
+++ src/pages/g6-pages/Messages.jsx
@@ -0,0 +1,12 @@
+// aqui será toda sua page 
+import Header from "./components/Header";
+import ConversationWrapper from "./components/ConversationWrapper";
+
+export default function Messages() {
+    return (
+        <>
+            <Header/>
+            <ConversationWrapper/>
+        </>
+    )
+}
