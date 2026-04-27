addae7cf2bd2ab17b1dcd0aa10aa2a37f8b9dd6d
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-04-27 10:57:42 -0300

    feat: component header

---
+++ src/components/g3/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+    width: 100%;
+    height: 75px;
+    background-color: #257E2B;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 50px;
+`;
