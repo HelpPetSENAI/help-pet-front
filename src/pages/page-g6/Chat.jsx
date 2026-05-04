fd907b82ac5938b37e79bb9d00994128a5afd723
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-04 08:03:08 -0300

    refactor: change this file to corret name folder

---
+++ src/pages/page-g6/Chat.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+
+export default function Chat() {
+    return (
+        <>
+            <Header/>
+            <Wrapper>
+                <h1>Pessoa</h1>
+            </Wrapper>
+        </>
+    )
+}
