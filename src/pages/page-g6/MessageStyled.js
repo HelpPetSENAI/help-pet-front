717cfd2ddffd8064880240e6be9ea63d2786c042
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-04 08:03:57 -0300

    fix: fixing styles

--- src/pages/page-g6/MessageStyled.js
+++ src/pages/page-g6/MessageStyled.js
@@ -1,19 +1,18 @@
 import styled from 'styled-components'
 
 export const MessagePageG6Styled = styled.main`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
 width: 100vw;  
 min-height: 100vh;
-padding: 32px;
 background: #f2f8f2;
 color: #1d3d2f;
 
   .page-content {
-    max-width: 740px;
-  }
-
-  h1 {
-    font-size: 2.4rem;
-    margin-bottom: 16px;
+    width: 100%;
   }
 
   p {
