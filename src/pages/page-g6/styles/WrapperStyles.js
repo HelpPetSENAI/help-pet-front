77de626accbdd2e26097888247df5298556d83f8
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-04 08:04:09 -0300

    fix: fixing styles

---
+++ src/pages/page-g6/styles/WrapperStyles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.section`
+    background-color: var(--clr-green-500);
+
+    .wrapper {
+        padding: 30px 20px 30px 20px;
+        font-family: var(--main-font);
+
+        width: 100%;
+        min-height: 100vh;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+
+        width: 100%;
+        height: 100%;
+
+        border: 2px solid var(--clr-green-1000);
+        border-radius: 30px 30px 0px 0px;
+
+        background-color: var(--clr-neutral-100);
+    }
+`
