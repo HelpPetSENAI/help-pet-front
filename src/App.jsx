b201e11941f8b51da86e289b664a758942e44ac4
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-04 07:59:40 -0300

    fix: fix paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,35 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ComingSoonPage from './pages/CommingSoonPage.jsx'
+import ComingSoonPage from './pages/CommingSoonPage'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 import GlobalStyle from './styles/GlobalStyle.js'
 import ExamplePage from './pages/ExamplePage.jsx'
-import Messages from './pages/g6-pages/Messages.jsx'
-import Chat from './pages/g6-pages/Chat.jsx'
+import MessagePageG6 from './pages/page-g6/MessagePageG6.jsx'
 
-// Criando uma array para definir as rotas do site utilizando a função createBrowserRouter importada
 const router = createBrowserRouter([
   {
     path: '/',
     element: <ComingSoonPage />,
-    // Error element aparece no caso de digitar um endereço que não existe, link com caminho errado ou erros de carregamento
-    // Apenas essa declaração de error element aparece em caso de erro em qualquer página
     errorElement: <NotFoundPage />
+  },
+  {
+    path: '/coming-soon',
+    element: <ComingSoonPage />
   },
   {
     path: '/example',
     element: <ExamplePage />
   },
   {
-    path: '/messages',
-    element: <Messages />
+    path: '/message',
+    element: <MessagePageG6 />
   },
-  {
-    path: '/messages/chat',
-    element: <Chat />
-  }
-
+  
 ])
 
 export default function App() {
-
   return (
     <>
       <GlobalStyle />
-      {/* RouterProvider fornece as rotas definidas acima para serem renderizadas através do atríbuto router */}
       <RouterProvider router={router} />
     </>
   )
