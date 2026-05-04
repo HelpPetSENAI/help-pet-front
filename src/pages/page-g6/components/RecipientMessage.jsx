bce59681e74bb83f52a1112f406d8be80874020c
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-04 20:50:35 -0300

    feat: recipient message component

---
+++ src/pages/page-g6/components/RecipientMessage.jsx
@@ -0,0 +1,9 @@
+import { StyledRecipientMessage } from '../styles/RecipientMessageStyles';
+
+export default function RecipientMessage() {
+    return (
+        <StyledRecipientMessage>
+            <p>a</p>
+        </StyledRecipientMessage>
+    )
+}
