11217a3522c3c85896af9f3b3f38b4830897865f
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-04 20:49:45 -0300

    feat: new chat page

--- src/pages/page-g6/Chat.jsx
+++ src/pages/page-g6/Chat.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
+import MessageInput from './components/MessageInput'
+import { StyledChat } from "./ChatStyled";
 
 export default function Chat() {
     return (
-        <>
+        <StyledChat>
             <Header/>
             <Wrapper>
                 <h1>Pessoa</h1>
             </Wrapper>
-        </>
+            <MessageInput />
+        </StyledChat>
     )
 }
