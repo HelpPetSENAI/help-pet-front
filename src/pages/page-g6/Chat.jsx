6c69b1278786ecf96fda39ab96bdc65ff92e3ac0
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-04 23:33:58 -0300

    feat: almost finish

--- src/pages/page-g6/Chat.jsx
+++ src/pages/page-g6/Chat.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 import MessageInput from './components/MessageInput'
 import { StyledChat } from "./ChatStyled";
+import SenderMessage from "./components/SenderMessage";
 
 export default function Chat() {
+    const [messages, setMessages] = useState([]);
+
+    function handleSendMessage(text) {
+        const newMessage = {
+            id: Date.now(),
+            text,
+            hour: new Date().toLocaleTimeString("pt-BR", {
+                hour: "2-digit",
+                minute: "2-digit",
+            }),
+        };
+
+        setMessages([...messages, newMessage]);
+    }
+
     return (
         <StyledChat>
             <Header/>
             <Wrapper>
-                <h1>Pessoa</h1>
+                <h1 className="recipient-username">Pessoa</h1>
+                <div className="chat-container">
+                    {messages.map((message) => (
+                        <SenderMessage
+                            key={message.id}
+                            text={message.text}
+                            hour={message.hour}
+                        />
+                    ))}
+                </div>
             </Wrapper>
-            <MessageInput />
+            <MessageInput onSendMessage={handleSendMessage} />
         </StyledChat>
     )
 }
