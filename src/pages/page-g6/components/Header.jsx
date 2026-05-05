9cc2d63bffce1a3e01c467264004af0c4a588606
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-04 23:35:17 -0300

    chore: add navigate to back arrow button

--- src/pages/page-g6/components/Header.jsx
+++ src/pages/page-g6/components/Header.jsx
@@ -1,11 +1,18 @@
 import { StyledHeader } from '../styles/HeaderStyles';
 import BackArrow from '../../../assets/icons/G6HeaderBackArrow';
 import Logo from '../../../assets/icons/G6HeaderLogo';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Header() {
+    const navigate = useNavigate();
+    const handleChatClick = () => {
+        navigate(-1);
+    }
+
     return (
         <StyledHeader>
-            <BackArrow className="back" aria-label="Voltar" />
+            <BackArrow className="back" aria-label="Voltar" onClick={handleChatClick} />
             <Logo className="logo" aria-label="Help Pet" />
             <div className="ghost"></div> {/* Elemento fantasma para alinhamento */}
         </StyledHeader>
