d2d809e52cb80ba3d53230b07154bdd4e0052ea4
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-04 23:34:05 -0300

    feat: almost finish

--- src/pages/page-g6/ChatStyled.js
+++ src/pages/page-g6/ChatStyled.js
@@ -5,4 +5,19 @@ export const StyledChat = styled.main`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    font-family: var(--main-font);
+    
+    .recipient-username { margin-bottom: 10px; }
+
+    .chat-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: flex-end;
+        gap: 4px;
+        padding: 0 16px 0 16px;
+    }
 `
