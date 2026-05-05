87db42a36f60c3a0b9422555f9335adf74b9f2cf
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-04 23:35:25 -0300

    feat: new component

--- src/pages/page-g6/components/MessageInput.jsx
+++ src/pages/page-g6/components/MessageInput.jsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import { StyledMessageInput } from '../styles/MessageInputStyles';
 import Send from '../../../assets/icons/SendMessageIcon'
 
-export default function MessageInput() {
+export default function MessageInput({ onSendMessage }) {
+    const [message, setMessage] = useState("");
+
+    function handleSend() {
+        if (message.trim() === "") return;
+
+        onSendMessage(message);
+        setMessage("");
+    }
+
+    function handleKeyDown(event) {
+        if (event.key === "Enter") {
+            handleSend();
+        }
+    }
+
     return (
         <StyledMessageInput>
-            <input type="text" />
-            <Send />
+            <input
+                type="text"
+                className='message-input'
+                placeholder='Digite aqui'
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            <Send className='send-icon' onClick={handleSend}/>
         </StyledMessageInput>
     )
 }
-
