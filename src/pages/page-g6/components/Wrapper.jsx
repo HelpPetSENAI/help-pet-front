c4b5346d93468633af1341eba8505dbd913191c0
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-05 18:34:28 -0300

    feat: className

--- src/pages/page-g6/components/Wrapper.jsx
+++ src/pages/page-g6/components/Wrapper.jsx
@@ -1,9 +1,9 @@
 import { StyledWrapper } from "../styles/WrapperStyles";
 
-export default function Wrapper({ children }) {
+export default function Wrapper({ children, className = '' }) {
     return (
         <StyledWrapper>
-            <section className="wrapper">
+            <section className={`wrapper ${className}`.trim()}>
                 {children}
             </section>
         </StyledWrapper>
