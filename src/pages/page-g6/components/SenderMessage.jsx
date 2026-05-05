a1dfed1d4f4c4e7659d0bd51c9a61683e099473b
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-04 23:35:35 -0300

    feat: new component

--- src/pages/page-g6/components/SenderMessage.jsx
+++ src/pages/page-g6/components/SenderMessage.jsx
@@ -1,9 +1,12 @@
 import { StyledSenderMessage } from '../styles/SenderMessageStyles';
 
-export default function SenderMessage() {
+export default function SenderMessage({ text, hour }) {
     return (
         <StyledSenderMessage>
-            <p>a</p>
+            <div className="message-box">
+                <p>{text}</p>
+            </div>
+            <div className="message-hour">{hour}</div>
         </StyledSenderMessage>
     )
 }
