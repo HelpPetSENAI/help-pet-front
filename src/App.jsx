7d5aa75fc46a3a280ffe3e3cc9875c4c7ceb487d
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-05-18 12:41:27 -0300

    refactor: reorganize page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import ComingSoonPage from "./pages/CommingSoonPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 import ExamplePage from "./pages/ExamplePage.jsx";
-import Login from "./pages/Login/Login.jsx";
-import Background from "./components/Background/Background.jsx";
+import Login from "./pages/pages-g1/Login/Login.jsx";
 
 import GlobalStyle from "./styles/GlobalStyle.js";
-import SignUp from "./pages/SignUp/SignUp.jsx";
+import SignUp from "./pages/pages-g1/SignUp/SignUp.jsx";
 import MessagePageG6 from './pages/page-g6/MessagePageG6.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <ComingSoonPage />,
-errorElement: <NotFoundPage />
+    errorElement: <NotFoundPage />
   },
   {
     path: '/coming-soon',
@@ -28,10 +26,6 @@ errorElement: <NotFoundPage />
   {
     path: '/message',
     element: <MessagePageG6 />
-  },  
-  {
-    path: "/login",
-    element: <Login />
   },
   
   {
